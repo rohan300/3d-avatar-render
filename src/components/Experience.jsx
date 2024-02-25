dc49564b3e860728db5214874fda0b583a2a4152
Rohan Rao <trohanrao@gmail.com>
2024-02-25 16:52:13 +0000

    Added initial 3d avatar

---
+++ src/components/Experience.jsx
@@ -0,0 +1,14 @@
+import { OrbitControls } from "@react-three/drei";
+import { Avatar } from "./Avatar";
+
+export const Experience = () => {
+  return (
+    <>
+      <OrbitControls />
+      <group position-y={-1}>
+      <Avatar />
+      </group> 
+      <ambientLight intensity={1} />
+    </>
+  );
+};
